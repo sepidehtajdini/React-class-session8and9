17545462caff26d0509422aeaec7c5bfd2129602
Sepideh <108736695+sepidehtajdini@users.noreply.github.com>
2022-12-12 06:22:02 -0800

    Add files via upload

---
+++ 8and9/Button/Button.js
@@ -0,0 +1,7 @@
+import "./button.css";
+function Button({text,className,onClick}){
+    return(
+        <button className={className} onClick={onClick}>{text}</button>
+    )
+}
+export default Button
